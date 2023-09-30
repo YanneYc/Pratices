8aab74d8d6967ed4f12610e3298c147c090ea28e
yanne tse <yanne065@gmail.com>
2023-09-30 02:46:40 +0100

    Add project files.

---
+++ Pratices/CollectionGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratices
+{
+    internal class CollectionGenerator
+    {
+        public static List<int> ListGenerateWithRandom(int count,int min , int max) 
+        {
+            Random rand = new Random();
+            int holder = 0;
+            List<int> result = new List<int>();
+            for (int i = 0; i < count; i++) 
+            {
+                holder = rand.Next(min,max);
+                result.Add(holder);
+            }
+            return result;
+        }
+        public static int[] ArrayGenerateWithRandom(int count , int min , int max)
+        {
+            Random rand = new Random();
+            int holder = 0;
+            int[] result = new int[count];
+            for (int i = 0; i < count; i++)
+            {
+                holder = rand.Next(min, max);
+                result[i] = holder;
+            }
+            return result;
+        }
+    }
+}
---
+++ Pratices/Program.cs
@@ -0,0 +1,26 @@
+﻿using Pratices;
+using Pratices.Algorithm;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+class Program 
+{
+    static void Main(string[] args) 
+    {
+        MinMax();
+    }
+    public static void MinMax() 
+    {
+        int[] arr = CollectionGenerator.ArrayGenerateWithRandom(10, 0, 100);
+        int max = FindMinMax.Start(arr, 0, arr.Length, true);
+        int min = FindMinMax.Start(arr, 0, arr.Length, false);
+        FindMinMax.PrintResult(arr, min, max);
+        List<int> list = CollectionGenerator.ListGenerateWithRandom(10, 0, 100);
+        max = FindMinMax.Start(list, 0, list.Count, true);
+        min = FindMinMax.Start(list, 0, list.Count, false);
+        FindMinMax.PrintResult(list, min, max);
+
+    }
+
+}
